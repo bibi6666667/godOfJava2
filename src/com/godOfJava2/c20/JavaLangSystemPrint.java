97ad5f353861a5f5391d3f29d6fbe1fd097dc322
bibi6666667 <bibi6666667@kakao.com>
2020-12-30 16:08:52 +0900

    [20장] System.out.println() - null 출력의 원리
    566-567
    
    - print()와 println()으로 객체를 출력하면, toString()이 호출되는 것이 아닌 String의 valueOf()가 호출되고 그 결과를 출력한다. 그래서 null인 객체도 잘 출력되는 것이다.
    - 두 번째 출력문은 null과 문자열을 합쳤는데 왜 예외가 발생하지 않을까? 컴파일러가 이 문장을 StringBuilder로 변환하기 때문. 29번 라인은 결과적으로 30번 라인과 같이 동작한다.
    
    - 이렇게 toString으로 호출해 출력하려 하면 예외가 발생한다 ( NullPointerException )
    - 왜? toString() 메소드는 null인 객체로 호출할 수 없기 때문.

--- src/com/godOfJava2/c20/JavaLangSystemPrint.java
+++ src/com/godOfJava2/c20/JavaLangSystemPrint.java
@@ -3,8 +3,9 @@ package com.godOfJava2.c20;
 public class JavaLangSystemPrint {
     public static void main(String[] args) {
         JavaLangSystemPrint sample = new JavaLangSystemPrint();
-        sample.printStreamCheck();
+        //sample.printStreamCheck();
         sample.printNull();
+        //sample.printNullToString();
     }
 
     public void printStreamCheck() {
@@ -22,6 +23,22 @@ public class JavaLangSystemPrint {
         System.out.println(value);
     }
 
-    
+    public void printNull() {
+        Object obj = null;
+        System.out.println(obj);
+        System.out.println(obj + " is object's value");
+        new StringBuilder().append(obj).append(" is object's value");
+        // print()와 println()으로 객체를 출력하면, toString()이 호출되는 것이 아닌 String의 valueOf()가 호출되고 그 결과를 출력한다.
+        // 그래서 null인 객체도 잘 출력되는 것이다.
+        // 두 번째 출력문은 null과 문자열을 합쳤는데 왜 예외가 발생하지 않을까? - 컴파일러가 이 문장을 StringBuilder로 변환하기 때문.
+        // 29번 라인은 결과적으로 30번 라인과 같이 동작한다.
+    }
+
+    public void printNullToString() {
+        Object obj = null;
+        System.out.println(obj.toString());
+        // 이렇게 toString으로 호출해 출력하려 하면 예외가 발생한다 - NullPointerException
+        // 왜? toString() 메소드는 null인 객체로 호출할 수 없기 때문.
+    }
 }
 
