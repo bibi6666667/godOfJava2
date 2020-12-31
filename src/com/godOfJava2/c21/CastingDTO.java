ecca9a40e2bddc9c5498eaf7111a66bcb763c8ad
bibi6666667 <bibi6666667@kakao.com>
2020-12-31 11:43:19 +0900

    [20장] DTO 클래스 - Data Transfer Object
    572-573
    
    - DTO : Data Transfer Object
    - 이 코드와 같이 'private 변수, getter, setter, Serializable' 구현을 해야만 제대로 된 DTO클래스라고 할 수 있다.
    - private변수 : private로 선언한 객체
    - getter, setter : get변수이름() 메소드, set변수이름()메소드.
    - Serializable : java.io 에서 import한 인터페이스.

---
+++ src/com/godOfJava2/c21/CastingDTO.java
@@ -0,0 +1,20 @@
+package com.godOfJava2.c21;
+
+import java.io.Serializable;
+
+public class CastingDTO {
+    private Object object;
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+    // DTO : Data Transfer Object
+    // 이 코드와 같이 'private 변수, getter, setter, Serializable' 구현을 해야만 제대로 된 DTO클래스라고 할 수 있다.
+    // private변수 : private로 선언한 객체
+    // getter, setter : get변수이름() 메소드, set변수이름()메소드.
+    // Serializable : java.io 에서 import한 인터페이스.
+}
