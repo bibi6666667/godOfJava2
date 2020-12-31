54a6f06bb28c331414c9cd9ccc17b37046d8cdad
bibi6666667 <bibi6666667@kakao.com>
2020-12-31 11:35:01 +0900

    [20장] 직접 해 봅시다 - 피드백
    568
    
    https://github.com/godofjava/GodOfJava2nd/blob/master/Chapter20/src/d/lang/practice/NumberObjects.java 의 정답을 보고 수정한 코드

--- src/com/godOfJava2/c20/NumberObjects.java
+++ src/com/godOfJava2/c20/NumberObjects.java
@@ -3,20 +3,23 @@ package com.godOfJava2.c20;
 public class NumberObjects {
     public static void main(String[] args) {
         NumberObjects numberObjects = new NumberObjects();
-        numberObjects.parseLong("010");
+
+        numberObjects.parseLong("r1024");
+        numberObjects.parseLong("1024");
         numberObjects.printOtherBase(1024);
     }
 
     public long parseLong(String data) {
-        long result;
+        long result = -1; // 에러일 때 리턴할 값 설정
         try {
-            result = Long.parseLong(data);
+            result = Long.parseLong(data); // 정상적일 때만 새로운 값 할당
             System.out.println(result);
-            return result;
-        } catch (NumberFormatException e) { // data가 숫자가 아닌 문자열일 떄
+        } catch (NumberFormatException ne) { // data가 숫자가 아닌 문자열일 떄
             System.out.println(data + " is not a number.");
-            return -1;
+        } catch (Exception e) {
+
         }
+        return result; // 정상적일 때는 할당된 값을, 에러일 때는 -1 리턴
     }
 
     public void printOtherBase(long value) {
