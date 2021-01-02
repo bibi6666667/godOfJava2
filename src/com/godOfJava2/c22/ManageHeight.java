5d5ff9c9ad06e6f8d31a9304d9353d3676689c8b
bibi6666667 <bibi6666667@kakao.com>
2021-01-02 22:23:20 +0900

    [22장] 직접 해 봅시다
    612-613
    
    열심히 만들었지만 ArrayList에 ArrayList를 넣는 게 처음이라 예시대로 동작하지 않는다ㅠ

--- src/com/godOfJava2/c22/ManageHeight.java
+++ src/com/godOfJava2/c22/ManageHeight.java
@@ -12,6 +12,11 @@ public class ManageHeight {
             System.out.println("Class No. : " + i);
             manageHeight.printHeight(i);
         }
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Class No. : " + i);
+            manageHeight.printAverage(i);
+        }
+
     }
 
     public void setData() {
@@ -21,31 +26,31 @@ public class ManageHeight {
         class1.add(173);
         class1.add(175);
         class1.add(177);
-        gradeHeights.add(class1);
+        gradeHeights.add(0, class1);
         ArrayList<Integer> class2 = new ArrayList<Integer>(4);
         class1.add(160);
         class1.add(165);
         class1.add(167);
         class1.add(186);
-        gradeHeights.add(class2);
+        gradeHeights.add(1, class2);
         ArrayList<Integer> class3 = new ArrayList<Integer>(4);
         class1.add(158);
         class1.add(177);
         class1.add(187);
         class1.add(176);
-        gradeHeights.add(class3);
+        gradeHeights.add(2, class3);
         ArrayList<Integer> class4 = new ArrayList<Integer>(3);
         class1.add(173);
         class1.add(182);
         class1.add(181);
-        gradeHeights.add(class4);
+        gradeHeights.add(3, class4);
         ArrayList<Integer> class5 = new ArrayList<Integer>(5);
         class1.add(170);
         class1.add(180);
         class1.add(165);
         class1.add(177);
         class1.add(172);
-        gradeHeights.add(class5);
+        gradeHeights.add(4, class5);
     }
 
     public void printHeight(int classNo) {
@@ -56,8 +61,15 @@ public class ManageHeight {
     }
 
     public void printAverage(int classNo) {
+        int sum = 0;
+        int students = 0;
         double classHeightAverage = 0;
-        double sum = gradeHeights.get(classNo - 1)
+        ArrayList<Integer> classHeights = gradeHeights.get(classNo - 1);
+        System.out.println(students);
+        for (int height : classHeights) {
+            sum += height;
+        }
+        classHeightAverage = sum / students;
         System.out.println("Class No. : " + classNo);
         System.out.println("Height average : " + classHeightAverage);
     }
