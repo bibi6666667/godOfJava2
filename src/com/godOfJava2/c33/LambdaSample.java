30414246134fea73644c9f889e349321755e6656
bibi6666667 <bibi6666667@kakao.com>
2021-01-18 16:21:31 +0900

    [33장] Lambda - 람다 표현식 연습
    빼기를 처리하는 람다표현식을 작성해 본다.

--- src/com/godOfJava2/c33/LambdaSample.java
+++ src/com/godOfJava2/c33/LambdaSample.java
@@ -3,7 +3,7 @@ package com.godOfJava2.c33;
 public class LambdaSample {
     public static void main(String[] args) {
         LambdaSample sample = new LambdaSample();
-        sample.calculateClassic();
+        sample.calculateLambda();
     }
 
     private void calculateClassic() { // Calculate인터페이스를 익명클래스로 구현하기.
@@ -22,6 +22,9 @@ public class LambdaSample {
         // (a, b) : Calculate 인터페이스는 메소드가 하나만 선언되어 있기 때문에, (a, b)는 operation()메소드의 int a와 int b를 매개변수로 받는다는 의미임.
         // -> a + b; : operation()메소드의 결과로 a와 b를 더한 값을 리턴한다는 의미임.
         // 변수명 (a, b)은 임의로 선언해도 상관없다.
+
+        Calculate calculateSubstract = (a, b) -> a - b; // 빼기를 처리하는 람다 표현식 만들어 보기.
+        System.out.println(calculateSubstract.operation(1, 2));
     }
 
 
