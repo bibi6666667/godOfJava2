6aecacb355974ec209f9adba6e771857a991df85
bibi6666667 <bibi6666667@kakao.com>
2021-01-21 17:29:55 +0900

    [25장] 쓰레드 - 쓰레드 생성법 2
     쓰레드 생성하는 법 2 - Thread클래스를 확장한다.

---
+++ src/com/godOfJava2/c25/ThreadSample.java
@@ -0,0 +1,8 @@
+package com.godOfJava2.c25;
+
+public class ThreadSample extends Thread{
+    public void run() {
+        System.out.println("This is ThreadSample's run() method.");
+    }
+    // 쓰레드 생성하는 법 2 - Thread클래스를 확장한다.
+}
