a58459bf3e1233a917490664bbb93fe655a723d2
bibi6666667 <bibi6666667@kakao.com>
2021-01-24 00:20:17 +0900

    [25장] 직접 해 봅시다 - 피드백
    https://github.com/godofjava/GodOfJava2nd/blob/master/Chapter25/src/e/thread/practice/TimerThread.java 의 답을 보고 수정한 코드.
    (TimerMain은 동일함)
    
    - 단순히 출력문 밀리초를 맞추는 게 아닌 sleep()시간을 조정해서 맞추어야 한다.
    - Date() 사용법이 다르다.

--- src/com/godOfJava2/c25/TimerThread.java
+++ src/com/godOfJava2/c25/TimerThread.java
@@ -5,13 +5,14 @@ import java.util.Date;
 public class TimerThread extends Thread {
 
     public void run() {
+        printCurrentTimeFirst();
         printCurrentTime();
     }
 
-    public void printCurrentTime() {
+    public void printCurrentTimeFirst() {
         for (int i = 0; i < 10; i++) {
             Date date = new Date();
-            System.out.print(date.toString());
+            System.out.print(date.toString() + " ");
             System.out.println(System.currentTimeMillis() - (System.currentTimeMillis() % 1000));
             try {
                 Thread.sleep(1000);
@@ -19,6 +20,30 @@ public class TimerThread extends Thread {
                 ie.printStackTrace();
             }
         }
+    }
 
+    public void printCurrentTime() {
+        try {
+            int count = 0;
+            while (count < 10) {
+                long currentTime = System.currentTimeMillis();
+                System.out.println(new Date(currentTime) + " " + currentTime);
+                Thread.sleep(900);
+                reduceTimeGap();
+                count++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reduceTimeGap() {
+        long currentTime = System.currentTimeMillis();
+        long timeMod = currentTime % 1000;
+        try {
+            Thread.sleep(1000 - timeMod);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
